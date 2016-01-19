2a128fc20021928c44a4b8e71106df251f8032a7
makmis <mixmax13@mail.ru>
2016-01-19 15:17:43 +0300

    дописал скрипт, который снимает SUID bit с заданных утилит и устаналвливать capabilities

--- permission.sh
+++ permission.sh
@@ -12,3 +12,20 @@ then
 	setcap cap_chon,cap_setuid+ep $CHFN
 fi
 
+# chsh
+echo -n "Change suid to capabilities for \"$CHSH\"? (y/n) "
+read item
+if [$item = y ];
+then
+        chmod u-s $CHSH
+        setcap cap_chown,cap_setuid+ep $CHSH
+fi
+
+# passwd
+echo -n "Change suid to capabilities for \"PASSWD\"? (y/n) "
+read item
+if [$item = y ];
+then
+        chmod u-s $PASSWD
+        setcap cap_chown,cap_dac_override,cap_fowner+ep $PASSWD
+fi
